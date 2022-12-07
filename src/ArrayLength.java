9879dc18eee232cac87a2365ab65c8a99a625f45
Martin-Turtle <info@martinturtle.co.uk>
2022-12-07 13:13:40 +0000

    Question 3 Array Length

---
+++ src/ArrayLength.java
@@ -0,0 +1,9 @@
+public class ArrayLength {
+    public static void main(String[] args) {
+        String[] cars ={"Volvo", "BMW", "Ford", "Mazda"};
+        System.out.println(cars.length);
+
+        int [] age = {8, 4, 6, 9};
+        System.out.println(age.length);
+    }
+}
